fdbb6cf34bac4b28f5dc03ac501d8e863512c409
niedbalski <niedbalski@users.noreply.github.com>
2023-10-25 14:47:22 +0000

    ci: update Core Operator chart from latest version internally
    Signed-off-by: GitHub <noreply@github.com>

--- charts/core-operator/test.sh
+++ charts/core-operator/test.sh
@@ -6,7 +6,7 @@ export MAX_ITERATIONS="${MAX_ITERATIONS:-10}"
 export RETRY_TIMEOUT="${RETRY_TIMEOUT:-2s}"
 
 echo "Creating a pipeline"
-if kubectl create -f - <<EOF
+if kubectl create -f - <<EOF; then
 apiVersion: core.calyptia.com/v1
 kind: Pipeline
 metadata:
@@ -39,102 +39,88 @@ spec:
         volume-size: 256Mi
 
 EOF
-then
-  echo "succeeded to create a pipeline"
+	echo "succeeded to create a pipeline"
 else
-  echo "ERROR: failed to create a pipline"
-  exit 1
+	echo "ERROR: failed to create a pipline"
+	exit 1
 fi
 
 echo "Waiting for pipeline deployment to come up"
-until kubectl rollout status -n "$NAMESPACE" deployment/"$PIPELINE_NAME"
-do
-  sleep "$RETRY_TIMEOUT"
+until kubectl rollout status -n "$NAMESPACE" deployment/"$PIPELINE_NAME"; do
+	sleep "$RETRY_TIMEOUT"
 done
 
 echo "Deleting pipeline deployment to come up"
-if kubectl delete deployment -n "$NAMESPACE" "$PIPELINE_NAME"
-then
-  echo "delete deployment succeeded"
+if kubectl delete deployment -n "$NAMESPACE" "$PIPELINE_NAME"; then
+	echo "delete deployment succeeded"
 else
-  echo "ERROR: delete deployment failed"
-  exit 1
+	echo "ERROR: delete deployment failed"
+	exit 1
 fi
 
 echo "Checking if pipline deployment has been recreated"
 
 counter=0
 
-while :
-do
-  counter=$((counter +1))
-  if [ "$counter" -ge "$MAX_ITERATIONS" ]
-  then
-    echo "ERROR: failed to get deployment after $counter retries"
-    exit 1
-  fi
-  if kubectl get deploy -n "$NAMESPACE" "$PIPELINE_NAME"
-  then
-    echo "deployment successfully recreated"
-    break
-  else
-    sleep "$RETRY_TIMEOUT"
-  fi
+while :; do
+	counter=$((counter + 1))
+	if [ "$counter" -ge "$MAX_ITERATIONS" ]; then
+		echo "ERROR: failed to get deployment after $counter retries"
+		exit 1
+	fi
+	if kubectl get deploy -n "$NAMESPACE" "$PIPELINE_NAME"; then
+		echo "deployment successfully recreated"
+		break
+	else
+		sleep "$RETRY_TIMEOUT"
+	fi
 done
 
 echo "delete config map"
-if kubectl delete configmap -n "$NAMESPACE" "$PIPELINE_NAME"
-then
-  echo "delete configmap succeeded"
+if kubectl delete configmap -n "$NAMESPACE" "$PIPELINE_NAME"; then
+	echo "delete configmap succeeded"
 else
-  echo "ERROR: delete configmap failed"
-  exit 1
+	echo "ERROR: delete configmap failed"
+	exit 1
 fi
 
 counter=0
 
-while :
-do
-  counter=$((counter +1))
-  echo "Retry nr $counter"
-  if [ "$counter" -ge "$MAX_ITERATIONS" ]
-  then
-    echo "ERROR: failed to get configmap after $counter retries"
-    exit 1
-  fi
-  if kubectl get configmap -n "$NAMESPACE" "$PIPELINE_NAME"
-  then
-    echo "configmap successfully recreated"
-    break
-  else
-    sleep "$RETRY_TIMEOUT"
-  fi
+while :; do
+	counter=$((counter + 1))
+	echo "Retry nr $counter"
+	if [ "$counter" -ge "$MAX_ITERATIONS" ]; then
+		echo "ERROR: failed to get configmap after $counter retries"
+		exit 1
+	fi
+	if kubectl get configmap -n "$NAMESPACE" "$PIPELINE_NAME"; then
+		echo "configmap successfully recreated"
+		break
+	else
+		sleep "$RETRY_TIMEOUT"
+	fi
 done
 
 counter=0
-while :
-do
-  counter=$((counter +1))
-  echo "Retry nr $counter"
-  if [ "$counter" -ge "$MAX_ITERATIONS" ]
-  then
-    echo "ERROR: failed to get service after $counter retries"
-    exit 1
-  fi
-  if kubectl get service -n "$NAMESPACE" "$PIPELINE_NAME"-tcp8888
-  then
-    echo "service successfully recreated"
-    break
-  else
-    sleep "$RETRY_TIMEOUT"
-  fi
+while :; do
+	counter=$((counter + 1))
+	echo "Retry nr $counter"
+	if [ "$counter" -ge "$MAX_ITERATIONS" ]; then
+		echo "ERROR: failed to get service after $counter retries"
+		exit 1
+	fi
+	if kubectl get service -n "$NAMESPACE" "$PIPELINE_NAME"-tcp8888; then
+		echo "service successfully recreated"
+		break
+	else
+		sleep "$RETRY_TIMEOUT"
+	fi
 done
 
-if kubectl delete pipeline -n "$NAMESPACE" "$PIPELINE_NAME"
-then
-  echo "removed the pipeline"
+if kubectl delete pipeline -n "$NAMESPACE" "$PIPELINE_NAME"; then
+	echo "removed the pipeline"
 else
-  echo "ERROR: failed removing the pipeline"
-  exit 1
+	echo "ERROR: failed removing the pipeline"
+	exit 1
 fi
 echo "Tests succeeded"
